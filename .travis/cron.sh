b1e3ed720d61515f6a63176245aeecfe3f319390
Seth Michael Larson <sethmichaellarson@gmail.com>
2019-07-31 19:58:21 -0500

    Update cron.sh

--- .travis/cron.sh
+++ .travis/cron.sh
@@ -4,10 +4,15 @@ set -x
 
 if nox -s build; then
   set -e
+  # Deal with detached head state that Travis puts us in
+  git checkout master
+
+  # Create a commit with the date attached
   datetime=`date "+%Y-%-m-%-d"`
   git add hstspreload/
   git commit -m "Automated updates to the HSTS preload list on $datetime"
 
+  # Use our GitHub token to make the commit
   git remote rm origin
   git remote add origin https://sethmlarson:${GITHUB_TOKEN}@github.com/sethmlarson/hstspreload > /dev/null 2>&1
   git push origin master --quiet
